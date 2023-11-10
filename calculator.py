bcc43e9f299d56ff8bec1c159ffc925aeb02b318
kanmanirock <kanmanirock@gmail.com>
2023-11-09 20:26:41 -0500

    Updated the code

--- calculator.py
+++ calculator.py
@@ -1,50 +1,60 @@
-def add(x, y):
+# Add type hints for the functions
+def add(x: float, y: float) -> float:
     return x + y
 
-def subtract(x, y):
+def subtract(x: float, y: float) -> float:
     return x - y
 
-def multiply(x, y):
+def multiply(x: float, y: float) -> float:
     return x * y
 
-def divide(x, y):
+def divide(x: float, y: float) -> float:
     if y != 0:
         return x / y
     else:
         raise ValueError("Cannot divide by zero")
 
-# Example usage:
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
 
-print("Select operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+def calculate(operation, num1, num2):
+    try:
+        if operation == '1':
+            return add(num1, num2), '+'
+        elif operation == '2':
+            return subtract(num1, num2), '-'
+        elif operation == '3':
+            return multiply(num1, num2), '*'
+        elif operation == '4':
+            return divide(num1, num2), '/'
+    except ValueError as e:
+        print(e)
+        return None, None
 
-choice = input("Enter choice (1/2/3/4): ")
-
-if choice in ['1', '2', '3', '4']:
-    if choice == '1':
-        result = add(num1, num2)
-        operation = '+'
-    elif choice == '2':
-        result = subtract(num1, num2)
-        operation = '-'
-    elif choice == '3':
-        result = multiply(num1, num2)
-        operation = '*'
-    else:
+def get_user_input(prompt):
+    while True:
         try:
-            result = divide(num1, num2)
-            operation = '/'
-        except ValueError as e:
-            print(e)
-            result = None
-            operation = None
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
-    if result is not None and operation is not None:
-        print(f"{num1} {operation} {num2} = {result}")
-else:
-    print("Invalid Input")
+def main():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice in ['1', '2', '3', '4']:
+        num1 = get_user_input("Enter the first number: ")
+        num2 = get_user_input("Enter the second number: ")
+
+        result, operation = calculate(choice, num1, num2)
+
+        if result is not None and operation is not None:
+            print(f"{num1} {operation} {num2} = {result}")
+    else:
+        print("Invalid Input")
+
+if __name__ == '__main__':
+    main()
